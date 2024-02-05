6518d1a20a72c4533ab687cef2a3ae6b70ec33c5
Yu Kitsune <kitsunegadget@gmail.com>
2024-02-05 15:00:44 +0900

    Update esbuild
    * esbuildの仕様変更により、configの条件分岐を対応

--- esbuild.config.js
+++ esbuild.config.js
@@ -2,51 +2,58 @@ import esbuild from "esbuild";
 import { copy } from "esbuild-plugin-copy";
 import { clean } from "esbuild-plugin-clean";
 
+const args = process.argv.slice(2);
+const settings = {
+  entryPoints: ["src/sw.ts", "src/popup/popup.ts"],
+  bundle: true,
+  outdir: "dist",
+  outbase: "src",
+  define: {
+    "process.env": "{}",
+    "process.env.NODE_ENV": ["--development", "--watch"].some((s) =>
+      args.includes(s)
+    )
+      ? `"development"`
+      : `"production"`,
+    // global: "window",
+  },
+  // pure: [""],
+  minify: args.includes("--production"),
+  metafile: true,
+  plugins: [
+    clean({ patterns: "./dist/*" }),
+    copy({
+      assets: [
+        {
+          from: ["./src/manifest.json", "./src/PTicon.png"],
+          to: ["./dist"],
+        },
+        {
+          from: ["./src/popup/popup.html"],
+          to: ["./popup/popup"],
+        },
+        {
+          from: ["./src/popup/images/*"],
+          to: ["./popup/images/images"],
+        },
+        {
+          from: ["./src/_locales/**/*"],
+          to: ["./_locales"],
+          keepStructure: true,
+        },
+      ],
+    }),
+  ],
+};
+
 (async () => {
-  const args = process.argv.slice(2);
-
-  const result = await esbuild.build({
-    entryPoints: ["src/sw.ts", "src/popup/popup.ts"],
-    bundle: true,
-    outdir: "dist",
-    outbase: "src",
-    define: {
-      "process.env": "{}",
-      "process.env.NODE_ENV": args.includes("--development")
-        ? `"development"`
-        : `"production"`,
-      // global: "window",
-    },
-    // pure: [""],
-    minify: args.includes("--production"),
-    metafile: true,
-    watch: args.includes("--watch"),
-    plugins: [
-      copy({
-        assets: [
-          {
-            from: ["./src/manifest.json", "./src/PTicon.png"],
-            to: ["./dist"],
-          },
-          {
-            from: ["./src/popup/popup.html"],
-            to: ["./popup/popup"],
-          },
-          {
-            from: ["./src/popup/images/*"],
-            to: ["./popup/images/images"],
-          },
-          {
-            from: ["./src/_locales/**/*"],
-            to: ["./_locales"],
-            keepStructure: true,
-          },
-        ],
-      }),
-      clean({ patterns: "./dist/*" }),
-    ],
-  });
-
-  const text = await esbuild.analyzeMetafile(result.metafile);
-  console.log(text);
+  if (args.includes("--watch")) {
+    const ctx = await esbuild.context(settings);
+    await ctx.watch();
+    console.log("watching...");
+  } else {
+    const result = await esbuild.build(settings);
+    const text = await esbuild.analyzeMetafile(result.metafile);
+    console.log(text);
+  }
 })();
